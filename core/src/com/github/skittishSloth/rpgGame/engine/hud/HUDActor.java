cd09fd523858f141b264e71f7f438587a7acf75a
Matthew Cory <Matthew.Cory2@bhnetwork.com>
2014-08-27 10:23:14 -0600

    Cleanup; more refactoring on HUD stuff.

--- core/src/com/github/skittishSloth/rpgGame/engine/hud/HUDActor.java
+++ core/src/com/github/skittishSloth/rpgGame/engine/hud/HUDActor.java
@@ -7,13 +7,9 @@
 package com.github.skittishSloth.rpgGame.engine.hud;
 
 import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.github.skittishSloth.rpgGame.engine.player.Player;
 
 /**
@@ -27,21 +23,7 @@ public class HUDActor extends Actor {
         
         this.player = player;
         
-        final Pixmap healthBarBoxPm = new Pixmap(104, 19, Pixmap.Format.RGBA8888);
-        healthBarBoxPm.setColor(0, 0, 0, 1);
-        healthBarBoxPm.drawRectangle(0, 0, 104, 19);
-        final Texture healthBarBoxTexture = new Texture(healthBarBoxPm);
-        healthBarBoxPm.dispose();
-        
-        healthBarBox = new Image(healthBarBoxTexture);
-
-        final Pixmap healthBarPm = new Pixmap(100, 15, Pixmap.Format.RGBA8888);
-        healthBarPm.setColor(1, 0, 0, 0.75f);
-        healthBarPm.fill();
-        final Texture healthBarTexture = new Texture(healthBarPm);
-        healthBarPm.dispose();
-
-        healthBar = new Image(healthBarTexture);
+        this.healthBarActor = new HealthBarActor(this.player);
     }
 
     @Override
@@ -49,27 +31,13 @@ public class HUDActor extends Actor {
         final Stage stage = getStage();
         final Camera camera = stage.getCamera();
         updateHealthBar(camera);
-        updateHealthBarBox(camera);
-        healthBarBox.draw(batch, parentAlpha);
-        healthBar.draw(batch, parentAlpha);
+        healthBarActor.draw(batch, parentAlpha);
     }
-
+    
     private void updateHealthBar(final Camera camera) {
-        final Vector3 screenCoords = new Vector3(2, healthBar.getHeight() + 2, 0);
-        final Vector3 worldCoords = camera.unproject(screenCoords);
-        healthBar.setX(worldCoords.x);
-        healthBar.setY(worldCoords.y);
-        healthBar.setScaleX(player.getHealthPercentage());
-    }
-
-    private void updateHealthBarBox(final Camera camera) {
-        final Vector3 screenCoords = new Vector3(0, healthBarBox.getHeight(), 0);
-        final Vector3 worldCoords = camera.unproject(screenCoords);
-        healthBarBox.setX(worldCoords.x);
-        healthBarBox.setY(worldCoords.y);
+        healthBarActor.update(camera);
     }
     
     private final Player player;
-    private final Image healthBarBox;
-    private final Image healthBar;
+    private final HealthBarActor healthBarActor;
 }
